8dbdbf27d32db2421470ed0391bce62e6f49766a
Hussein-Mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2023-10-02 18:41:24 +0300

    Added Button and NavBar components

---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <button
+    @click="onClick()"
+    :class="['rounded-sm bg-[#5165FD] px-4 py-2 text-white', classes]"
+    :disabled="disabled"
+  >
+    <slot></slot>
+  </button>
+</template>
+<script>
+export default {
+  props: {
+    classes: String,
+    disabled: Boolean
+  },
+  methods: {
+    onClick() {
+      this.$emit('btn-click')
+    }
+  }
+}
+</script>
+<style lang=""></style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,49 @@
+<script setup>
+import Button from './Button.vue'
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <nav class="px-6 py-5 shadow-sm">
+    <div class="flex items-center justify-between">
+      <button @click="toggleMenu()">
+        <img src="@/assets/imgs/mobile_burger_menu.svg" alt="burger menu" />
+      </button>
+      <div></div>
+      <!-- <form action=""></form>
+    <div class="flex w-96 items-center justify-between">
+      <h1>الرئيسية</h1>
+      <h1>من نحن</h1>
+      <h1>راسلنا</h1>
+    </div> -->
+      <RouterLink to="/">
+        <div class="h-9 w-10">
+          <img src="@/assets/imgs/logo.png" alt="Qorra logo" />
+        </div>
+      </RouterLink>
+    </div>
+    <div :class="[{ hidden: !showMenu }, 'py-10 text-xl']">
+      <ul>
+        <li class="pb-11">الرئيسية</li>
+        <li class="pb-11">من نحن</li>
+        <li class="pb-11">راسلنا</li>
+      </ul>
+      <Button classes="px-11">تطوع معنا</Button>
+    </div>
+  </nav>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      showMenu: false
+    }
+  },
+  methods: {
+    toggleMenu() {
+      this.showMenu = !this.showMenu
+    }
+  }
+}
+</script>
+<style lang=""></style>
